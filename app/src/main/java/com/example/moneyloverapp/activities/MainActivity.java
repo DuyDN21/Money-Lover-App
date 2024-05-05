464655ff1a706cc180ea6b99ca3e2005bd5294af
DuyDN <dongocduy.duydo@gmail.com>
2024-05-05 11:39:14 +0700

    [feature]: wallet list UI

--- app/src/main/java/com/example/moneyloverapp/activities/MainActivity.java
+++ app/src/main/java/com/example/moneyloverapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.moneyloverapp;
+package com.example.moneyloverapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -8,11 +8,19 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.moneyloverapp.R;
+import com.example.moneyloverapp.models.Wallet;
+import com.example.moneyloverapp.recycleViews.WalletList.WalletListAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.moneyloverapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        //set up bottom navigation
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.budgetFragment,
                 R.id.dashboardFragment,
@@ -37,5 +49,17 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //wallet recycler view
+        RecyclerView walletListRV = findViewById(R.id.wallet_list);
+
+        List<Wallet> walletList = new ArrayList<Wallet>();
+        walletList.add(new Wallet("Lương", 8000000, R.drawable.wallet_icon));
+        walletList.add(new Wallet("Bố mẹ chu cấp", 2000000, R.drawable.wallet_icon));
+        walletList.add(new Wallet("Học bổng", 10000000, R.drawable.wallet_icon));
+
+        walletListRV.setLayoutManager(new LinearLayoutManager(this));
+        walletListRV.setAdapter(new WalletListAdapter(getApplicationContext(), walletList));
+
     }
 }
--- app/src/main/java/com/example/moneyloverapp/fragments/TransactionFragment.java
+++ app/src/main/java/com/example/moneyloverapp/fragments/TransactionFragment.java
@@ -1,4 +1,4 @@
-package com.example.moneyloverapp;
+package com.example.moneyloverapp.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.moneyloverapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
